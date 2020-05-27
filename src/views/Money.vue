b568a6f1d6e2555b150c8b8975784d6ec4e81195
chuzhong <352995096@qq.com>
2020-05-27 12:56:33 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+    </div>
     <Nav/>
   </div>
 
@@ -11,3 +38,18 @@
     name: 'Money',
   };
 </script>
+
+<style lang="scss" scoped>
+  .nav-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .content {
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1;
+  }
+</style>
