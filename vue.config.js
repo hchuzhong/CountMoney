a1a36c32263d504c15783010b96276fb67271f27
chuzhong <352995096@qq.com>
2020-05-27 23:19:59 +0800

    config.js 修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const path = import('path')
+/* eslint-disable */
+const path = require('path')
 
 module.exports = {
   lintOnSave: false,
@@ -10,7 +11,7 @@ module.exports = {
       .test(/\.svg$/)
       .include.add(dir).end() // 包含 icons 目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
-    config.plugin('svg-sprite').use(import('svg-sprite-loader/plugin'), [{plainSprite: true}])
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
 
   }
