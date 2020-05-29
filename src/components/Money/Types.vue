8773e25abe81a12016d8880752b1401dfb0155fa
chuzhong <352995096@qq.com>
2020-05-29 18:01:45 +0800

    完成 Types 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,41 +1,39 @@
 <template>
   <ul class="types">
     <li :class="type === '-' && 'selected'"
-        @click="selectType('-')">支出</li>
+        @click="selectType('-')">支出
+    </li>
     <li :class="type === '+' && 'selected'"
-        @click="selectType('+')">收入</li>
+        @click="selectType('+')">收入
+    </li>
   </ul>
 </template>
 
-<script lang="js">
-  export default {
-    name: 'Types',
-    props: ['xxx'],
-    data(){
-      return{
-        type: '-' //'-' 表示支出, '+' 表示收入
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Types extends Vue {
+    type = '-';   //'-' 表示支出, '+' 表示收入
+
+    selectType(type: string) {
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown');
       }
-    },
-    mounted(){
-      console.log(this.xxx)
-    },
-    methods: {
-      selectType(type){
-        if(type !== '-' && type !== '+'){
-          throw new Error('type is unknown')
-        }
-        this.type = type
-      }
+      this.type = type;
     }
-  };
+  }
+
 </script>
 
 <style lang="scss" scoped>
-  .types{
+  .types {
     background: #c4c4c4;
     display: flex;
     text-align: center;
     font-size: 24px;
+
     > li {
       width: 50%;
       height: 64px;
@@ -43,7 +41,8 @@
       justify-content: center;
       align-items: center;
       position: relative;
-      &.selected::after{
+
+      &.selected::after {
         content: '';
         position: absolute;
         bottom: 0;
