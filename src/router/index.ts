141855a21023b39d9eac69f4be828a3761cae29e
chuzhong <352995096@qq.com>
2020-06-01 11:50:46 +0800

    点击标签可以跳转到编辑页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
-import EditLable from '@/views/EditLable.vue';
+import EditLabel from '@/views/EditLabel.vue';
 
 Vue.use(VueRouter);
 
@@ -26,8 +26,8 @@ const routes: Array<RouteConfig> = [
     component: Statistics
   },
   {
-    path: '/labels/edit',
-    component: EditLable
+    path: '/labels/edit/:id',
+    component: EditLabel
   },
   {
     path: '*',
