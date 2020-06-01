199a2c64aed8a3fe8152d6cb1278dbbec3fb9444
chuzhong <352995096@qq.com>
2020-06-01 17:38:24 +0800

    修复 value 可以通过外部赋值的 bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Layout class-prefix="layout">
+      {{record}}
       <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
       <Types :value.sync="record.type"/>
       <div class="notes">
