f806d1111847a3bdf0a15c3fd0affe3b0328eca4
chuzhong <352995096@qq.com>
2020-06-01 17:47:03 +0800

    展示 tag 的 name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
       <Button>删除标签</Button>
@@ -26,13 +26,15 @@
     components: {Button, FormItem}
   })
   export default class EditLabel extends Vue {
+    tag?: { id: string; name: string } = undefined;
+
     created() {
       const id = this.$route.params.id;
-      tagListModel.fetch;
+      tagListModel.fetch();
       const tags = tagListModel.data;
       const tag = tags.filter(t => t.id === id)[0];
       if (tag) {
-        console.log(tag);
+        this.tag = tag;
       } else {
         this.$router.replace('/404');
       }
@@ -41,7 +43,7 @@
 </script>
 
 <style lang="scss" scoped>
-  .navBar{
+  .navBar {
     text-align: center;
     font-size: 16px;
     padding: 12px 16px;
@@ -49,22 +51,27 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
-    >.title{
+
+    > .title {
     }
-    >.leftIcon{
+
+    > .leftIcon {
       width: 24px;
       height: 24px;
     }
-    >.rightIcon{
+
+    > .rightIcon {
       width: 24px;
       height: 24px;
     }
   }
-  .form-wrapper{
+
+  .form-wrapper {
     background: white;
     margin-top: 8px;
   }
-  .button-wrapper{
+
+  .button-wrapper {
     text-align: center;
     padding: 16px;
     margin-top: 44-16px;
