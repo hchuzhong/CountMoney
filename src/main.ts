15be336165c3c19b82fa2a9165676995c87b53a1
chuzhong <352995096@qq.com>
2020-06-02 18:10:17 +0800

    封装 tagList 的增删改查

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,13 @@ import tagListModel from '@/models/tagListModel';
 Vue.config.productionTip = false;
 
 Vue.component('Nav', Nav);
-Vue.component('Layout', Layout)
-Vue.component('Icon',Icon)
+Vue.component('Layout', Layout);
+Vue.component('Icon', Icon);
 
 window.tagList = tagListModel.fetch();
+window.findTag = (id) => {
+  return window.tagList.filter(t => t.id === id)[0];
+}
 window.createTag = (name: string) => {
   const message = tagListModel.create(name);
   if (message === 'duplicated') {
@@ -22,6 +25,12 @@ window.createTag = (name: string) => {
   } else if (message === 'success') {
     window.alert('成功添加标签');
   }
+};
+window.removeTag = (id) => {
+  return tagListModel.remove(id);
+};
+window.updateTag = (id, name) => {
+  return tagListModel.update(id, name);
 }
 
 new Vue({
