37f417cead792abc35a2cd6a2aa2b81b04011c4b
chuzhong <352995096@qq.com>
2020-06-06 14:41:55 +0800

    修复 Money.vue 页面的样式，删除 Statistics.vue 中按天、周、月的标签

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,18 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-    <div>
-      type:{{type}}
-      <br/>
-      interval: {{interval}}
-    </div>
+    <ol>
+      <li v-for="(group, index) in groupList" :key="index">
+        <h3 class="title">{{beautify(group.title)}}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -16,31 +22,100 @@
   import Tabs from '@/components/Tabs.vue';
   import intervalList from '@/constants/intervalList';
   import typeList from '@/constants/typeList';
+  import dayjs from 'dayjs';
+  import clone from '@/lib/clone';
 
   @Component({
     components: {Tabs}
   })
   export default class Statistics extends Vue {
+    beautify(string: string) {
+      const day = dayjs(string);
+      const now = dayjs();
+      if (day.isSame(now, 'day')) {
+        return '今天';
+      } else if (day.isSame(now.subtract(1,'day'), 'day')) {
+        return '昨天';
+      } else if (day.isSame(now.subtract(2,'day'), 'day')) {
+        return '前天';
+      } else if(day.isSame(now, 'year')){
+        return day.format('M月D日');
+      } else {
+        return day.format('YYYY年M月D日');
+      }
+    }
+
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? '无' : tags.join('，');
+    }
+
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
+    }
+
+    get groupList() {
+      const {recordList} = this;
+      if (recordList.length === 0 ){return [];}
+      const newList = clone(recordList).sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
+      const result = [{title: dayjs(newList[0].createdAt).format('YYYY-M-D'), items: [newList[0]]}]
+      for (let i=1; i<newList.length; i++){
+        const current = newList[i]
+        const last = result[result.length-1]
+        if(dayjs(last.title).isSame(dayjs(current.createdAt), 'day')){
+          last.items.push(current)
+        } else {
+          result.push({title: dayjs(newList[i].createdAt).format('YYYY-M-D'), items: [current]})
+        }
+      }
+      console.log(result);
+      return result;
+    }
+
+    beforeCreate() {
+      this.$store.commit('fetchRecords');
+    }
+
     type = '-';
-    interval = 'day';
-    intervalList = intervalList;
     typeList = typeList;
   }
 </script>;
 
 <style scoped lang="scss">
-  ::v-deep .type-tabs-item {
-    background: white;
-
-    &.selected {
+  ::v-deep {
+    .type-tabs-item {
       background: #c4c4c4;
 
-      &::after {
-        display: none;
+      &.selected {
+        background: white;
+
+        &::after {
+          display: none;
+        }
       }
     }
   }
-  ::v-deep .interval-tabs-item{
-    height: 48px;
+
+  %item {
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+
+  .title {
+    @extend %item;
+  }
+
+  .record {
+    background: white;
+    @extend %item;
+  }
+
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    overflow: scroll;
+    color: #999;
   }
 </style>
