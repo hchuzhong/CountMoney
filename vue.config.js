46d36315e0345329c8ba6cff7d496cc2f91e10ff
chuzhong <352995096@qq.com>
2020-06-07 17:41:40 +0800

    修复一些小 bug

--- vue.config.js
+++ vue.config.js
@@ -6,18 +6,27 @@ module.exports = {
     ? '/morney-website/'
     : '/',
   lintOnSave: false,
-  chainWebpack: config =>{
+  chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
 
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() // 包含 icons 目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
 
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
   }
 }
