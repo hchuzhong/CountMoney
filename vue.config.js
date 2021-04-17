f782a5d05ae57728321ebac44693a76a2e4b8ff2
hchuzhong <352995096@qq.com>
2021-04-17 19:27:04 +0800

    修改发布地址

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/morney-website/'
+    ? '/CountMoney-website/'
     : '/',
   lintOnSave: false,
   chainWebpack: config => {
@@ -13,10 +13,10 @@ module.exports = {
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() // 包含 icons 目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({ extract: false }).end()
       .use('svgo-loader').loader('svgo-loader')
-      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+      .tap(options => ({ ...options, plugins: [{ removeAttrs: { attrs: 'fill' } }] })).end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{ plainSprite: true }])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
 
   },
